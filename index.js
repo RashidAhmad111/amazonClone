a85b4aacc63f9ed26c8a0ea99ef040ca300f03a6
RashidAhmad111 <159822331+RashidAhmad111@users.noreply.github.com>
2024-02-13 10:06:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+button=document.getElementById("papoo").onclick=function(){
+    console.log(window .open("https://www.amazon.in/?&ext_vrnc=hi&tag=googhydrabk1-21&ref=pd_sl_7hz2t19t5c_e&adgrpid=58355126069&hvpone=&hvptwo=&hvadid=676742234347&hvpos=&hvnetw=g&hvrand=9661347059368362875&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1007824&hvtargid=kwd-10573980&hydadcr=14453_2367553&gclid=Cj0KCQiApOyqBhDlARIsAGfnyMqD5Akf1shxuQKNicufuXkizVR4ooWEOOUItvhYUZbRsPTnqjoywUgaAoUXEALw_wcB") )
+}
+button=document.getElementById("too").onclick=function(){
+    age=18
+    if(age>18)
+    alert("YOU ARE ADULT SIGN IN ")
+    else{
+        ("you are not adult ")
+    }
+}
+let =document.getElementById("a").onclick=function(){
+    alert("TODAY IS AVAILABLE")
+
+
+}
+let =document.getElementById("b").onclick=function(){
+    alert(" AVAILABLE ")
+
+
+}
+let =document.getElementById("c").onclick=function(){
+    alert("REGISTRY PLEASE SIGN IN")
+
+
+}
+let =document.getElementById("d").onclick=function(){
+    alert ("ONLY FOR RASHID AHMAD GIFTS")
+    style="red"
+
+
+}
+let =document.getElementById("e").onclick=function(){
+    alert("CELL")
+
+
+}
+let =document.querySelector(".box").onclick=function(){
+    let=document.querySelector(".search-bar border").innerHTML="HEALTH AND PERSONAL CARE"
+
+
+}
